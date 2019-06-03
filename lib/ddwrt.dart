281dd5674ed1ef40c01e75db61c2d77e6c5ec195
Aaron Madlon-Kay <aaron@madlon-kay.com>
2019-06-03 22:43:24 +0900

    Implement VPN toggle

--- lib/ddwrt.dart
+++ lib/ddwrt.dart
@@ -20,8 +20,36 @@ class DdWrt {
     String pass,
   ) async {
     final uri = Uri.http(host, '/Status_OpenVPN.asp');
+    return http.get(uri, headers: _basicAuth(user, pass));
+  }
+
+  Future<http.Response> toggleVpn(
+    String host,
+    String user,
+    String pass,
+    bool enabled,
+  ) {
+    final value = enabled ? 1 : 0;
+    return applyUser(host, user, pass, {
+      'openvpncl_enable': value.toString(),
+      'submit_button': 'PPTP',
+      'action': 'ApplyTake'
+    });
+  }
+
+  Future<http.Response> applyUser(
+    String host,
+    String user,
+    String pass,
+    Map<String, String> data,
+  ) {
+    final uri = Uri.http(host, '/applyuser.cgi');
+    return http.post(uri, headers: _basicAuth(user, pass), body: data);
+  }
+
+  Map<String, String> _basicAuth(String user, String pass) {
     final authUtf8 = utf8.encode('$user:$pass');
     final authBase64 = base64Url.encode(authUtf8);
-    return http.get(uri, headers: {'Authorization': 'Basic $authBase64'});
+    return {'Authorization': 'Basic $authBase64'};
   }
 }
